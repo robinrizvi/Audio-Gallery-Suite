ec76c395f55db016a82cef797d9ca8afef47d7ea
Robin Rizvi <mail@robinrizvi.info>
2012-07-07 21:21:31 +0530

    Replaced jquery-css-transform.js with a newer updated version
    The old version did not work for IE and thus caused a bug when using AGS
    in IE (the spinning cd did not spin)

--- Gallery/script/jquery-css-transform.js
+++ Gallery/script/jquery-css-transform.js
@@ -1,11 +1,14 @@
 (function ($) {
     // Monkey patch jQuery 1.3.1+ css() method to support CSS 'transform'
-    // property uniformly across Webkit/Safari/Chrome and Firefox 3.5.
-    // 2009 Zachary Johnson www.zachstronaut.com
+    // property uniformly across Safari/Chrome/Webkit, Firefox 3.5+, IE 9+, and Opera 11+.
+    // 2009-2011 Zachary Johnson www.zachstronaut.com
+    // Updated 2011.05.04 (May the fourth be with you!)
     function getTransformProperty(element)
     {
         // Try transform first for forward compatibility
-        var properties = ['transform', 'WebkitTransform', 'MozTransform'];
+        // In some versions of IE9, it is critical for msTransform to be in
+        // this list before MozTranform.
+        var properties = ['transform', 'WebkitTransform', 'msTransform', 'MozTransform', 'OTransform'];
         var p;
         while (p = properties.shift())
         {
@@ -19,9 +22,29 @@
         return 'transform';
     }
     
+    var _propsObj = null;
+    
     var proxied = $.fn.css;
-    $.fn.css = function (arg)
+    $.fn.css = function (arg, val)
     {
+        // Temporary solution for current 1.6.x incompatibility, while
+        // preserving 1.3.x compatibility, until I can rewrite using CSS Hooks
+        if (_propsObj === null)
+        {
+            if (typeof $.cssProps != 'undefined')
+            {
+                _propsObj = $.cssProps;
+            }
+            else if (typeof $.props != 'undefined')
+            {
+                _propsObj = $.props;
+            }
+            else
+            {
+                _propsObj = {}
+            }
+        }
+        
         // Find the correct browser specific property and setup the mapping using
         // $.props which is used internally by jQuery.attr() when setting CSS
         // properties via either the css(name, value) or css(properties) method.
@@ -29,9 +52,9 @@
         // need a DOM node to find the right CSS property, and there is some risk
         // that somebody would call the css() method before body has loaded or any
         // DOM-is-ready events have fired.
-		if
+        if
         (
-			typeof $.props['transform'] == 'undefined'
+            typeof _propsObj['transform'] == 'undefined'
             &&
             (
                 arg == 'transform'
@@ -43,7 +66,7 @@
             )
         )
         {
-            $.props['transform'] = getTransformProperty(this.get(0));
+            _propsObj['transform'] = getTransformProperty(this.get(0));
         }
         
         // We force the property mapping here because jQuery.attr() does
@@ -51,9 +74,37 @@
         // but curCSS() does *not* do property mapping when *getting* a
         // CSS property.  (It probably should since it manually does it
         // for 'float' now anyway... but that'd require more testing.)
-        if (arg == 'transform')
+        //
+        // But, only do the forced mapping if the correct CSS property
+        // is not 'transform' and is something else.
+        if (_propsObj['transform'] != 'transform')
         {
-            arg = $.props['transform'];
+            // Call in form of css('transform' ...)
+            if (arg == 'transform')
+            {
+                arg = _propsObj['transform'];
+                
+                // User wants to GET the transform CSS, and in jQuery 1.4.3
+                // calls to css() for transforms return a matrix rather than
+                // the actual string specified by the user... avoid that
+                // behavior and return the string by calling jQuery.style()
+                // directly
+                if (typeof val == 'undefined' && jQuery.style)
+                {
+                    return jQuery.style(this.get(0), arg);
+                }
+            }
+
+            // Call in form of css({'transform': ...})
+            else if
+            (
+                typeof arg == 'object'
+                && typeof arg['transform'] != 'undefined'
+            )
+            {
+                arg[_propsObj['transform']] = arg['transform'];
+                delete arg['transform'];
+            }
         }
         
         return proxied.apply(this, arguments);
